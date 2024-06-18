c972308e0534c22b4be04483fe298180656e020c
srikanth369 <vadlakondasrikanth136@gmail.com>
2024-06-18 14:15:46 +0530

    AnalogClock files Added

---
+++ Analog.js
@@ -0,0 +1,31 @@
+// Get references to dom elements
+const body = document.querySelector("body"),
+hourHand = document.querySelector(".hour"),
+minuteHand = document.querySelector(".minute"),
+secondHand = document.querySelector(".second"),
+modeswitch = document.querySelector(".mode-switch");
+
+modeswitch.addEventListener("click", () =>{
+    body.classList.toggle("Dark");
+    const isDarkMode =body.classList.contains("dark");
+    modeswitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+    localStorage.selectItem("mode", isDarkMode ? "Dark Mode" : "Light Mode")
+
+});
+
+const updateTime = () =>{
+    
+    let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360;
+    minToDeg = (date.getMinutes() / 60) * 360;
+    hrToDeg = (date.getHours() / 12) * 360;
+    
+
+    secondHand.style.transform = `rotate(${secToDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+    hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
